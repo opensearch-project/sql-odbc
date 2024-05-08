ca344491e4c5bcb96d2d314f167286a9c66dd228
fepatrifork <fepa@trifork.com>
2024-05-08 09:42:55 +0200

    Prepare the branch for merge the fork
    Signed-off-by: fepatrifork <fepa@trifork.com>

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -19,9 +19,7 @@ Set-Location $CURRENT_DIR
 # Build driver
 $DRIVER_SOURCE_DIR = "${WORKING_DIR}\src"
 $DRIVER_BUILD_DIR = "${BUILD_DIR}\odbc\cmake"
-
-# Insert here the proper folder of the vcpkg installed in the local environmnent
-$VCPKG_INSTALLED_DIR = "c:\odbcdriver\vcpkg_installed\$env:VCPKG_DEFAULT_TRIPLET"
+$VCPKG_INSTALLED_DIR = "${DRIVER_SOURCE_DIR}\vcpkg_installed\$env:VCPKG_DEFAULT_TRIPLET"
 
 .\scripts\build_driver.ps1 `
     $CONFIGURATION $WIN_ARCH `
